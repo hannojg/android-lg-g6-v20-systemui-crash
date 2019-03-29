e115b237fad58924a9d308ff8fe01e44b868c69a
hannojg <h.gdk@cernischi-und-goedecke.de>
2019-03-29 16:28:18 +0100

    added basic action items to notification

--- app/src/main/java/de/hannogoedecke/lgcrashpattern/MusicNotificationService.java
+++ app/src/main/java/de/hannogoedecke/lgcrashpattern/MusicNotificationService.java
@@ -12,20 +12,26 @@ import de.hannogoedecke.lgcrashpattern.notifications.NotificationImpl;
 public class MusicNotificationService extends Service {
 
     public static final String ACTION_NOTIFICATION_CRASH = ".action.notificationcrash";
+    public static final String ACTION_TOGGLE_PLAYBACK = ".action.TOGGLE_PLAYBACK";
+    public static final String ACTION_STOP = ".action.STOP";
+    public static final String ACTION_NEXT = ".action.NEXT";
+    public static final String ACTION_PREVIOUS = ".action.PREVIOUS";
 
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        BaseNotification notification = null;
+        BaseNotification notification;
         if (intent.getAction().equals(ACTION_NOTIFICATION_CRASH)) {
-            Toast.makeText(getApplicationContext(), "CRASH!", Toast.LENGTH_SHORT).show();
             notification = new NotificationImpl();
-        }
 
-        startForeground(
-                BaseNotification.NOTIFICATION_ID,
-                notification.createNotification(getApplicationContext())
-        );
+            startForeground(
+                    BaseNotification.NOTIFICATION_ID,
+                    notification.createNotification(getApplicationContext())
+            );
+        } else {
+            // Notification actions
+            Toast.makeText(getApplicationContext(), intent.getAction(), Toast.LENGTH_SHORT).show();
+        }
 
         return START_STICKY;
     }
--- app/src/main/java/de/hannogoedecke/lgcrashpattern/notifications/NotificationImpl.java
+++ app/src/main/java/de/hannogoedecke/lgcrashpattern/notifications/NotificationImpl.java
@@ -7,7 +7,13 @@ import android.content.Intent;
 
 import androidx.core.app.NotificationCompat;
 import de.hannogoedecke.lgcrashpattern.MainActivity;
+import de.hannogoedecke.lgcrashpattern.MusicNotificationService;
 import de.hannogoedecke.lgcrashpattern.R;
+
+import static de.hannogoedecke.lgcrashpattern.MusicNotificationService.ACTION_NEXT;
+import static de.hannogoedecke.lgcrashpattern.MusicNotificationService.ACTION_PREVIOUS;
+import static de.hannogoedecke.lgcrashpattern.MusicNotificationService.ACTION_STOP;
+import static de.hannogoedecke.lgcrashpattern.MusicNotificationService.ACTION_TOGGLE_PLAYBACK;
 
 public class NotificationImpl implements BaseNotification {
 
@@ -42,6 +48,38 @@ public class NotificationImpl implements BaseNotification {
                         PendingIntent.FLAG_UPDATE_CURRENT
                         ));
 
+        setActionItems(context, mBuilder);
+
+        mBuilder.setContentTitle("LG Notification Crash Demo");
+        mBuilder.setSubText("DEMO");
+
         return mBuilder.build();
+    }
+
+    private void setActionItems(Context context, NotificationCompat.Builder mBuilder) {
+        Intent nextIntent = new Intent(context, MusicNotificationService.class);
+        nextIntent.setAction(ACTION_NEXT);
+        PendingIntent pendingNextIntent = PendingIntent.getService(context, 100, nextIntent, 0);
+
+        nextIntent.setAction(ACTION_PREVIOUS);
+        PendingIntent pendingPreviousIntent = PendingIntent.getService(context, 100, nextIntent, 0);
+
+        Intent stopIntent = new Intent(context, MusicNotificationService.class);
+        stopIntent.setAction(ACTION_STOP);
+        PendingIntent stopPendingIntent = PendingIntent.getService(context, 100, stopIntent, 0);
+
+        Intent toggleIntent = new Intent(context, MusicNotificationService.class);
+        toggleIntent.setAction(ACTION_TOGGLE_PLAYBACK);
+        PendingIntent pendingTogglePlaybackIntent = PendingIntent.getService(context, 100, toggleIntent, 0);
+
+        NotificationCompat.Action actionClose = new NotificationCompat.Action(R.drawable.ic_close_white_24dp, "Close", stopPendingIntent);
+        NotificationCompat.Action actionPrev = new NotificationCompat.Action(R.drawable.ic_skip_previous_white_24dp, "Previous", pendingPreviousIntent);
+        NotificationCompat.Action actionToggle = new NotificationCompat.Action(R.drawable.ic_pause_white_24dp,"PlayToggle.Pausing", pendingTogglePlaybackIntent);
+        NotificationCompat.Action actionNext = new NotificationCompat.Action(R.drawable.ic_skip_next_white_24dp, "Next", pendingNextIntent);
+
+        mBuilder.mActions.add(0, actionClose);
+        mBuilder.mActions.add(1, actionPrev);
+        mBuilder.mActions.add(2, actionToggle);
+        mBuilder.mActions.add(3, actionNext);
     }
 }
